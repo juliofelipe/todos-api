c17adfc1a38bdb08ee9880809f476dcbdd5ad552
Julio Felipe <julio@informach.com.br>
2022-07-11 05:50:55 -0300

    feat: test post and get user ok

--- src/index.js
+++ src/index.js
@@ -15,10 +15,10 @@ function checksExistsUserAccount(request, response, next) {
   const user = users.find(user => user.username === username);
 
   if( !user) {
-    return response.status(400).json({error: "Username does not exist"});
+    return response.status(400).json({error: "User not found"});
   }
 
-  request.user = user;
+  request.username = username;
 
   return next();
 }
@@ -26,32 +26,37 @@ function checksExistsUserAccount(request, response, next) {
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
 
+
   const userAlreadyExists = users.some(
     (user) => user.username === username
   );
 
   if(userAlreadyExists) {
-    return response.status(400).json({error: "Username already exists"});
+    return response.status(400).json({error: "User already Exists"});
   }
 
-  users.push({
+  const user = {
+    id: uuidv4(),
     name,
     username,
-    id: uuidv4(),
     todos: [],
-  });
+  };
 
-  return response.status(201).send();
+  users.push(user);
+
+  return response.status(201).json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  const { user } = request;
+  const { username } = request;
+
+  const user = users.find(user => user.username === username);
 
   return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
